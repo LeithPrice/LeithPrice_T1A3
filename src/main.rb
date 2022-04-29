9521ae490b30455dd701226eab493d8865e2f3c0
LeithPrice <leith.price@gmail.com>
2022-04-30 01:57:15 +1000

    1 method left

--- src/main.rb
+++ src/main.rb
@@ -1,7 +1,10 @@
+# Initializing the Gems
+
 require 'colorize'
 require 'artii'
 require 'tty-prompt'
 
+# clearing all variables and arrays
 
 $workshop_name = ''
 $workshop_address = ''
@@ -9,15 +12,23 @@ $time = ''
 $customer_details = []
 $prompt =TTY::Prompt.new
 $a = Artii::Base.new
-$value =''
+$value = ''
+$file_data = []
+$input = ''
 
-def initilize(workshop_name, workshop_address, time)
+# initializing the arrays and variables to global status
+
+def initilize(workshop_name, workshop_address, time, value, input)
         $workshop_name = workshop_name
         $workshop_address = workshop_address
         $time = time
         $value = value
+        $input = input
         $customer_details = []
+        $file_data = []
 end
+
+# the method for the banner display at top of every page
 
 def banner_title
     system "clear"
@@ -29,12 +40,14 @@ def banner_title
     puts " "
 end
 
+# the method for the leaving program question
+
 def leave_software
+    puts = " "
     value = $prompt.select("Would you like to leave the Software?") do |menu|
         menu.choice "Yes", 1
         menu.choice "No", 2
     end 
-   
     if value == 1
         banner_title
         puts "Thank you for using #{$workshop_name} Workshop Software"
@@ -44,9 +57,11 @@ def leave_software
     end
 end
 
+# the method for displaying final invoice 
+
 def display_invoice
     banner_title    
-    totalresult = $customer_details[7] * $time
+    totalresult = $customer_details[6] * $time
     puts "*******************************************************************".colorize(:green)
     puts "*******************************************************************".colorize(:green)
     puts "******* Work Invoice **********************************************".colorize(:green)
@@ -56,11 +71,11 @@ def display_invoice
     puts " Vehicle Year     : #{$customer_details[3]}"
     puts " Vehicle Make     : #{$customer_details[4]}"
     puts " Vehicle Model    : #{$customer_details[5]}"
-    puts " Odometer         : #{$customer_details[6]} kms"
+    puts " Odometer         : #{$customer_details[7]} kms"
     puts "*******************************************************************".colorize(:green)
     puts "*******************************************************************".colorize(:green)
     print " Labour Hourly Rate :"
-    puts "           $#{$customer_details[7]}".colorize(:blue)
+    puts "           $#{$customer_details[6]}".colorize(:blue)
     print " Labour Quantity    :"
     puts "           #{$time}".colorize(:blue)
     puts "_____________________________________________".colorize(:red)
@@ -70,8 +85,9 @@ def display_invoice
     puts " "
     puts "*******************************************************************".colorize(:green)
     leave_software
-
 end
+
+# the method for calculating the labour time to charge
 
 def labour_time
     banner_title
@@ -86,77 +102,91 @@ def labour_time
 
 end
 
+# the method for entering the odometer of the vehicle
+
+def odometer_input
+    banner_title
+        puts "Odometer Kms:"
+        begin
+            input = gets.chomp
+            input = Integer(input)
+        rescue ArgumentError
+             puts "Please enter correct odometer:".colorize(:red)
+        retry
+    end
+    $customer_details.push input
+end
+
+# the method for inputing customer information into an array and saving data to the text file
 
 def customer_information
     banner_title
     $customer_details.clear
     puts "Customer Name:"
-    input = gets.chomp.to_s
-    $customer_details.push input
+    $input = gets.chomp.to_s
+    $customer_details.push $input
     banner_title
     puts "Customer Address:"
-    input = gets.chomp.to_s
-    $customer_details.push input
+    $input = gets.chomp.to_s
+    $customer_details.push $input
     banner_title
     puts "Vehicle Registration Number:"
-    input = gets.chomp.to_s
-    $customer_details.push input
+    $input = gets.chomp.upcase
+    $customer_details.push $input
     banner_title
     puts "Vehicle Year:"
     begin
-        input = gets.chomp
-        input = Integer(input)
+        $input = gets.chomp
+        $input = Integer($input)
     rescue ArgumentError
-        banner_title
         puts "Please enter correct year:".colorize(:red)
         retry
     end
-    $customer_details.push input
+    $customer_details.push $input
     banner_title
     puts "Vehicle Make:"
-    input = gets.chomp.to_s
-    $customer_details.push input
+    $input = gets.chomp.to_s
+    $customer_details.push $input
     banner_title
     puts "Vehicle Model:"
-    input = gets.chomp.to_s
-    $customer_details.push input
+    $input = gets.chomp.to_s
+    $customer_details.push $input
     banner_title
-    puts "Odometer Kms:"
+    puts "What is the Hourly Labour Rate for this Customer ($):"
     begin
-        input = gets.chomp
-        input = Integer(input)
+        $input = gets.chomp
+        $input = Float($input)
     rescue ArgumentError
-        
-        puts "Please enter correct odometer:".colorize(:red)
-        retry
-    end
-    $customer_details.push input
-    banner_title
-    puts "What is the Hourly Labour Rate for this Customer:"
-    begin
-        input = gets.chomp
-        input = Float(input)
-    rescue ArgumentError
-        
         puts "Please enter correct Hourly Rate".colorize(:red)
         retry
     end
-    $customer_details.push input
+    $customer_details.push $input
     File.write("customer.txt", $customer_details, mode: "a")
 end
 
-
-
+# the method for getting user input for searching the text file for existing customers and saving to a usable array
 
 def existing_customer
     banner_title
- file_data = File.read("customer.txt")
-print file_data
-
-
+    puts "Vehicle Registration Number:"
+    input = gets.chomp.upcase
+    arr = []
+    $customer_details.clear
+    $file_data = File.read("customer.txt")
+    puts $file_data
+    #  $file_data.select { |arr| arr[2] == "#{input}" }
+    $customer_details.push arr
+    puts $customer_details
+    puts input
+    while ($customer_details == nil)
+        if ($customer_details == nil)
+        existing_customer
+        else ($customer_details != nil)
+    end
+end
 end
 
-
+# the method for asking if user wants to generate a new invoice and if the customer is new or old
 
 def invoice_query
     banner_title
@@ -172,10 +202,14 @@ def invoice_query
         end 
         if value == 1
             customer_information
+            odometer_input
             labour_time
             display_invoice
         elsif value == 2
             existing_customer
+            # odometer_input
+            # labour_time
+            # display_invoice
         end
     elsif value == 2
         banner_title
@@ -183,15 +217,13 @@ def invoice_query
     end
 end
 
-
+# initial logon code 
 
 system "clear" 
 puts "Welcome to Workshop Software".colorize(:red)
 puts "----------------------------".colorize(:blue)
 puts "Please enter your Workshop Name"
 $workshop_name= gets.chomp.to_s
-puts "Please enter your Workshop Address"
-$workshop_address = gets.chomp.to_s
 banner_title
 invoice_query
 
